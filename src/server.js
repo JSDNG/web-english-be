887f5414166d8234ae6a919cb1a68f46a9c99ef8
JSDNG <tranducquynh00000@gmail.com>
2024-03-24 10:04:31 +0700

    configViewEngine-route

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
-const express = require("express"); //commonjs
-const path = require("path"); //commonjs
 require("dotenv").config();
+const express = require("express"); //commonjs
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hardcode
 const hostname = process.env.HOST_NAME;
 
 // config template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+configViewEngine(app);
 
-// config static files
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  //res.send("Hello World! & nodemon");
-  res.render("sample.ejs");
-});
-
+// khai bao route
+app.use("/", webRoutes);
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
