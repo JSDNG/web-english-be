14766be148236dc01a048531742af8eb92cf0826
dev <tranducquynh00000@gmail.com>
2024-04-09 10:11:08 +0700

    connection database

--- src/server.js
+++ src/server.js
@@ -7,11 +7,28 @@ const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hardcode
 const hostname = process.env.HOST_NAME;
 
+const mysql = require("mysql2");
 // config template engine
 configViewEngine(app);
 
 // khai bao route
 app.use("/", webRoutes);
+
+// test connection
+// create the connection to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "123456",
+    database: "huongdichvu",
+});
+
+// simple query
+connection.query("SELECT * FROM Users", function (err, results, fields) {
+    console.log(results);
+});
+
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`);
 });
