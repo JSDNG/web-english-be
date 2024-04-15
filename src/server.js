627b0baf4e553c6b01e0d27eebc4d82d4cf5fc24
dev <tranducquynh00000@gmail.com>
2024-04-15 10:14:49 +0700

    config connection mysql

--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-
+const connection = require("./config/database");
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hardcode
 const hostname = process.env.HOST_NAME;
 
-const mysql = require("mysql2");
 // config template engine
 configViewEngine(app);
 
@@ -15,18 +14,10 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 // test connection
-// create the connection to database
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    user: "root",
-    password: "123456",
-    database: "huongdichvu",
-});
 
 // simple query
-connection.query("SELECT * FROM Users", function (err, results, fields) {
-    console.log(results);
+connection.query("SELECT * FROM user", function (err, results, fields) {
+    console.log(">>>>", results);
 });
 
 app.listen(port, hostname, () => {
